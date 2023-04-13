2d10d885712e96e9dbe67344a29ada308b465670
AdryanMoreira <adriano.moreira@totvs.com.br>
2023-04-13 18:25:15 -0300

    projeto iniciado

---
+++ src/test/java/com/adryan/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adryan.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
