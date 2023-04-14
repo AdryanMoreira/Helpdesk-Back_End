6a2602b63dd4bb501ab71638d9e57a9cfe24ab89
AdryanMoreira <adriano.moreira@totvs.com.br>
2023-04-14 16:46:30 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/adryan/helpdesk/domain/Chamado.java
+++ src/main/java/com/adryan/helpdesk/domain/Chamado.java
@@ -5,19 +5,41 @@ import java.util.Objects;
 
 import com.adryan.helpdesk.domain.enums.Prioridade;
 import com.adryan.helpdesk.domain.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Chamado {
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
 	public Chamado() {
 		super();
 	}
