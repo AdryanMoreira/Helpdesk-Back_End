303c5f10318f523a5b7150b41627603ab830b30a
AdryanMoreira <adriano.moreira@totvs.com.br>
2023-04-15 21:18:53 -0300

    continuacao do curso

---
+++ src/main/java/com/adryan/helpdesk/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package com.adryan.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adryan.helpdesk.domain.Tecnico;
+import com.adryan.helpdesk.repositories.TecnicoRepository;
+import com.adryan.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired 
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto nao encontrado! Id :" + id));
+	}
+}
